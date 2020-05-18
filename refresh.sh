e86c703918e0e9696fd5374bd97255ab6e81fd29
Phil Leggetter <phil@leggetter.co.uk>
2020-05-18 23:45:45 +0100

    Check if the latest nodejs version is already installed

--- refresh.sh
+++ refresh.sh
@@ -6,5 +6,16 @@
 # asdf
 asdf plugin-add nodejs
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring;
-asdf install nodejs latest
-asdf global nodejs $(asdf latest nodejs)
+
+if [[ $(node -v) != *$(asdf latest nodejs) ]]; then
+    asdf install nodejs latest
+    asdf global nodejs $(asdf latest nodejs)
+else
+    echo "Latest version of nodejs already installed"
+fi
+
+# Yeoman
+npm install -g yo
+
+# yo repo (for license, code of conduct)
+npm install -g generator-repo
