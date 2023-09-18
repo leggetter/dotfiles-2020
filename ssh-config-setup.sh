13742817c0b3e5a288bf71091cfdf930484c37b7
Phil Leggetter <phil@leggetter.co.uk>
2023-09-18 19:18:27 +0100

    update ssh config

--- ssh-config-setup.sh
+++ ssh-config-setup.sh
@@ -5,24 +5,24 @@ if [ ! -d "$HOME/.ssh" ]; then
     mkdir $HOME/.ssh
 fi
 
-while ! [[ -f $HOME/.ssh/id_rsa && -f $HOME/.ssh/id_rsa.pub ]]
+while ! [[ -f $HOME/.ssh/id_ed25519 && -f $HOME/.ssh/id_ed25519.pub ]]
 do
-    if [ -f $HOME/.ssh/id_rsa ]; then
+    if [ -f $HOME/.ssh/id_ed25519 ]; then
         echo "id_rsa exists"
     fi
 
-    if [ -f $HOME/.ssh/id_rsa.pub ]; then
+    if [ -f $HOME/.ssh/id_ed25519.pub ]; then
         echo "id_rsa.pub exists"
     fi
 
-    echo "Please add your private and public keys to ${HOME}/.ssh/"
+    echo "Please add your private (id_ed25519) and public keys (id_ed25519.pub) to ${HOME}/.ssh/"
 
     read -p "Press any key to continue... " -n1 -s
 done
 
 # Add to keychain 
 # Will be prompted for passphrase, if one exists
-ssh-add -K $HOME/.ssh/id_rsa
+ssh-add -K $HOME/.ssh/id_ed25519
 ssh-add -A
 
 # Config that means passphrase won't be asked for again
@@ -35,5 +35,5 @@ ln -s $HOME/.dotfiles/ssh-config $HOME/.ssh/config
 
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/config
-chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_ed25519
+chmod 644 ~/.ssh/id_ed25519.pub
